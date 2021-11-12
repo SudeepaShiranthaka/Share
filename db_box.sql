dcc8267cfeb8f3bf515799eb5fa86e3688a94427
Sudeepa Shiranthaka <58376903+SudeepaShiranthaka@users.noreply.github.com>
2021-11-12 23:28:32 +0530

    new db

--- db_box.sql
+++ db_box.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 08, 2018 at 07:32 AM
--- Server version: 10.1.24-MariaDB
--- PHP Version: 7.1.6
+-- Generation Time: Nov 12, 2021 at 06:57 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -40,7 +39,11 @@ CREATE TABLE `blog` (
 
 INSERT INTO `blog` (`blog_id`, `title`, `content`) VALUES
 (1, 'The Little Gingerbread Man', 'Once upon a time there was an old woman who loved baking gingerbread. She would bake gingerbread cookies, cakes, houses and gingerbread people, all decorated with chocolate and peppermint, caramel candies and colored frosting.'),
-(2, 'The Halloween House', 'Now Suzie\'s moved in--she\'s only 4--along with her brother, her father and mother, and little Picador. He\'s their dog. Well, maybe half a dog. He\'s a Chihuahua, as small as they come.\r\n\r\nSuzie\'s room is in the attic. It\'s no fun. With a high ceiling, cold and gloomy, and shadows that run halfway up the walls. Suzie hides under the blanket. Picador too. Come on, he\'s no guard dog.');
+(2, 'The Halloween House', 'Now Suzie\'s moved in--she\'s only 4--along with her brother, her father and mother, and little Picador. He\'s their dog. Well, maybe half a dog. He\'s a Chihuahua, as small as they come.\r\n\r\nSuzie\'s room is in the attic. It\'s no fun. With a high ceiling, cold and gloomy, and shadows that run halfway up the walls. Suzie hides under the blanket. Picador too. Come on, he\'s no guard dog.'),
+(6, 'XFILTRATION-CTF{w3bs1t3s_c4n_b3_34s1ly_d3f4c3d_w1t', '<script>alert(12)</script>'),
+(7, '<script>alert(12)</script>', 'XFILTRATION-CTF{w3bs1t3s_c4n_b3_34s1ly_d3f4c3d_w1th_xss}'),
+(10, '<script>alert(22)</script>', '<script>alert(22)</script>'),
+(12, '<script>alert(30)</script>', '<script>alert(30)</script>');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +63,8 @@ ALTER TABLE `blog`
 -- AUTO_INCREMENT for table `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `blog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `blog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
